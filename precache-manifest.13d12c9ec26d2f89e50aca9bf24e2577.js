72adf5d4b57b7e43e4fca6a4ac121cca033d2e3a
MuhammadMuneeb <94845611+MuhammadMuneeb1@users.noreply.github.com>
2022-07-12 04:39:10 -0700

    Updates

---
+++ precache-manifest.13d12c9ec26d2f89e50aca9bf24e2577.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafcdac58a5ccfe5d63ed29deb9ca49e",
+    "url": "/Sound-Shop/index.html"
+  },
+  {
+    "revision": "9b7a54b4659293666ce2",
+    "url": "/Sound-Shop/static/css/2.b68f3192.chunk.css"
+  },
+  {
+    "revision": "b9d2d7c126bf99ab092a",
+    "url": "/Sound-Shop/static/css/main.f7f1dbdf.chunk.css"
+  },
+  {
+    "revision": "9b7a54b4659293666ce2",
+    "url": "/Sound-Shop/static/js/2.177eda8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sound-Shop/static/js/2.177eda8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d2d7c126bf99ab092a",
+    "url": "/Sound-Shop/static/js/main.e8ea4b99.chunk.js"
+  },
+  {
+    "revision": "ee6859b7f885e1ed5635",
+    "url": "/Sound-Shop/static/js/runtime-main.2d18a900.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Sound-Shop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Sound-Shop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Sound-Shop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Sound-Shop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Sound-Shop/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
